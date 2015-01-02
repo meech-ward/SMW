266054be250d7f3b3819b0defc3e4641bf8656fc
Sam Meech-Ward <sam@meech-ward.com>
2015-01-02 17:55:50 +0000

    Initial Commit

---
+++ SMW/SMW.h
@@ -0,0 +1,13 @@
+//
+//  SMW.h
+//  SMW
+//
+//  Created by Sam Meech Ward on 2015-01-02.
+//  Copyright (c) 2015 Sam Meech-Ward. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SMW : NSObject
+
+@end
---
+++ SMW/SMW.m
@@ -0,0 +1,13 @@
+//
+//  SMW.m
+//  SMW
+//
+//  Created by Sam Meech Ward on 2015-01-02.
+//  Copyright (c) 2015 Sam Meech-Ward. All rights reserved.
+//
+
+#import "SMW.h"
+
+@implementation SMW
+
+@end
